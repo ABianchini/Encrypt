7a226ba80bbf5769f8b5dd2db5b3502af8d368d3
Adriano Bianchini <animationbureau@gmail.com>
2012-05-04 06:27:30 -0700

    swapping spaces with strings

--- Encrypt/src/com/advancementbureau/encrypt/DecryptOne.java
+++ Encrypt/src/com/advancementbureau/encrypt/DecryptOne.java
@@ -10,6 +10,8 @@ public class DecryptOne {
 		File inputFile = new File("output.txt");
 		Scanner in = new Scanner(inputFile);
 		PrintWriter out = new PrintWriter("outputOne.txt");
+		String codeWord = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.";
+		int spaceCount = 0;
 		
 		while (in.hasNextLine()) {
 			String inLine = in.nextLine();
@@ -17,8 +19,14 @@ public class DecryptOne {
 			char[] inLinePieces = inLine.toCharArray();
 			char[] outLinePieces = new char[inLinePieces.length];
 			for (int i = 0; i < inLinePieces.length; i++) {
+				char outChar;
 				char inChar = inLinePieces[i];
-				char outChar = (char) (inChar - 1);
+				if (inChar == codeWord.charAt(spaceCount)) {
+					outChar = ' ';
+					spaceCount++;
+				} else {
+					outChar = (char) (inChar - 1);
+				}
 				outLinePieces[i] = outChar;
 			}
 			String outLine = new String(outLinePieces);
--- Encrypt/src/com/advancementbureau/encrypt/EncryptOne.java
+++ Encrypt/src/com/advancementbureau/encrypt/EncryptOne.java
@@ -10,15 +10,23 @@ public class EncryptOne {
 		File inputFile = new File("inputTest.txt");
 		Scanner in = new Scanner(inputFile);
 		PrintWriter out = new PrintWriter("output.txt");
+		String codeWord = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.";
+		int spaceCount = 0;
 		
 		while (in.hasNextLine()) {
 			String inLine = in.nextLine();
-			//String outLine = "";
 			char[] inLinePieces = inLine.toCharArray();
 			char[] outLinePieces = new char[inLinePieces.length];
 			for (int i = 0; i < inLinePieces.length; i++) {
+				
+				char outChar;
 				char inChar = inLinePieces[i];
-				char outChar = (char) (inChar + 1);
+				if (inChar == ' ') {
+					outChar = codeWord.charAt(spaceCount);
+					spaceCount++;
+				} else {
+					outChar = (char) (inChar + 1);
+				}
 				outLinePieces[i] = outChar;
 			}
 			String outLine = new String(outLinePieces);
